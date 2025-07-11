405b40f9d21faef04d3c202efb1b28478c625518
TrevourCodz0 <iauetdn@gmail.com>
2025-07-12 00:08:27 +0100

    Update Login.jsx

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const res = await fetch('http://localhost:5000/auth/login', {
+      const res = await fetch('https://spin2winapi.onrender.com/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
