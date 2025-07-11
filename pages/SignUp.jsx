00d19a754ff7896c7eedcb5b40df4fbf637564bc
TrevourCodz0 <iauetdn@gmail.com>
2025-07-12 00:08:03 +0100

    Update SignUp.jsx

--- pages/SignUp.jsx
+++ pages/SignUp.jsx
@@ -11,7 +11,7 @@ const Signup = () => {
   const handleSignup = async (e) => {
     e.preventDefault();
     try {
-      const res = await fetch('http://localhost:5000/auth/signup', {
+      const res = await fetch('https://spin2winapi.onrender.com/auth/signup', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
